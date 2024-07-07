52d40b3db1d1f9169e2adac70f54926b8251c28b
Masaaki Manabe <62591431+Romasa102@users.noreply.github.com>
2024-07-07 08:37:47 +0800

    Can get the tokens now

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -4,27 +4,54 @@ import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sig
 import 'package:provider/provider.dart';
 import 'UserData.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AuthService {
   final GoogleSignIn _googleSignIn = GoogleSignIn(
     scopes: <String>[
       googleAPI.CalendarApi.calendarScope,
     ],
+    clientId: '213698548031-5elgmjrqi6vof2nos67ne6f233l5t1uo.apps.googleusercontent.com',
   );
 
   Future<GoogleSignIn?> signInWithGoogle(BuildContext context) async {
     try {
-      await _googleSignIn.signIn().then((result) {
-        result?.authentication.then((googleKey) {
-          print(googleKey.accessToken);
-          print(googleKey.idToken);
-          print(_googleSignIn.currentUser);
-        }).catchError((err) {
-          print('inner error');
-        });
-      }).catchError((err) {
-        print('error occured');
-      });
+      final result = await _googleSignIn.signIn();
+      if (result == null) {
+        print('Google Sign-In aborted');
+        return null;
+      }
+
+      final googleKey = await result.authentication;
+      final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key=AIzaSyDGdPfqhFIZRdy-Y8-_QOJ072rSwnCTcxo');
+
+      final response = await http.post(
+        url,
+        headers: {'Content-type': 'application/json'},
+        body: jsonEncode({
+          'postBody': 'id_token=${googleKey.idToken}&providerId=google.com',
+          'requestUri': 'http://localhost',
+          'returnIdpCredential': true,
+          'returnSecureToken': true
+        }),
+      );
+
+      if (response.statusCode != 200) {
+        throw 'Refresh token request failed: ${response.statusCode}';
+      }
+
+      final data = Map<String, dynamic>.of(jsonDecode(response.body));
+      if (data.containsKey('refreshToken')) {
+        // Here is your refresh token, store it in a secure way
+        print('Refresh Token: ${data['refreshToken']}');
+      } else {
+        throw 'No refresh token in response';
+      }
+
+      print('Access Token: ${googleKey.accessToken}');
+      print('ID Token: ${googleKey.idToken}');
+      print('Current User: ${_googleSignIn.currentUser}');
 
       await _googleSignIn.authenticatedClient();
       Provider.of<UserData>(context, listen: false).updateGoogleUser(_googleSignIn);
