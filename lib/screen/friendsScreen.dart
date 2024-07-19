0c091338b170b145bbb0e7b8cf3a1ed83dc0fb4f
黒狗さん。 <kuroinusanshinji@gmail.com>
2024-07-19 14:12:23 +0900

    フレンドスクリーン中身作成

--- lib/screen/friendsScreen.dart
+++ lib/screen/friendsScreen.dart
@@ -9,17 +9,16 @@ class FriendsScreen extends StatefulWidget {
   _FriendsScreenState createState() => _FriendsScreenState();
 }
 
-class People {}
-
 class _FriendsScreenState extends State<FriendsScreen> {
-
+  Future<List<String>>? friends;
   @override
   void initState() {
     super.initState();
     GoogleSignIn? gUser = Provider.of<UserData>(context, listen: false).googleUser;
     if (gUser?.currentUser != null) {
       //ここでフレンド一覧を取得する
-
+      friends = GetFriends().getFriends("woody_1227");
+      //friends = GetFriends().getFriends(gUser?.currentUser!.id);
     }
   }
 
@@ -41,9 +40,35 @@ class _FriendsScreenState extends State<FriendsScreen> {
         padding: EdgeInsets.all(20),
         child: Column(
           children: [
-            Text('フレンド一覧'),
             //フレンド一覧を表示する
-            ListView.builder(),
+            FutureBuilder<List<String>>(
+              future: friends,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting){
+                  return CircularProgressIndicator();
+                }else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }else if (snapshot.hasData){
+                  return Container(
+                    child: Expanded(
+                      child:ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return ListTile(
+                                title: Text(
+                                  snapshot.data![index],
+                                  style: TextStyle(fontSize: 25),
+                                ),
+                              );
+                            }
+                        ),
+                    ),
+                  );
+                }else{
+                  return Text('No data');
+                }
+              },
+            ),
           ],
         ),
       ),
