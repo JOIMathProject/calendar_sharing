8ea4de012cf22d54598c63dcffcb8cc39dccde5e
黒狗さん。 <kuroinusanshinji@gmail.com>
2024-07-25 09:22:32 +0900

    フレンドプロファイルの見た目だけを完成

--- lib/screen/friendProfile.dart
+++ lib/screen/friendProfile.dart
@@ -14,9 +14,69 @@ class _FriendProfileState extends State<FriendProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Friend Profile'),
+        //ボタン
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.block,
+              color: Colors.red,
+              size: 30,
+            ),
+            onPressed: () {
+              //フレンド削除のアクション
+            },
+          ),
+        ],
       ),
-      body: Text("something"),
+      body: Center(
+        child: Column(
+          //真ん中に寄せる
+          children: [
+            //アイコン
+            SizedBox(height: 20),
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.grey,
+            ),
+            //名前
+            SizedBox(height: 20),
+            Text(
+              widget.friend.uname,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            //uid
+            SizedBox(height: 20),
+            Text(
+              "uid:"+widget.friend.uid,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            Row(
+              //ボタンを横に並べる
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //コンテンツのボタンとメッセージのボタン
+                ElevatedButton(
+                  onPressed: () {
+                    //コンテンツのボタンのアクション
+                  },
+                  child: Icon(Icons.content_copy),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    //メッセージのボタンのアクション
+                  },
+                  child: Icon(Icons.message),
+                ),
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
