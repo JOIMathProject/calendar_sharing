e3bc69d48991a2deba4f6064db4247f3023f5945
Masaaki Manabe <62591431+Romasa102@users.noreply.github.com>
2024-08-31 00:42:46 +0800

    color issue addressed

--- lib/setting/color.dart
+++ lib/setting/color.dart
@@ -11,15 +11,15 @@ Color Unselected = Color(0xFFBCBCBC);
 final appTheme = ThemeData(
   colorScheme: const ColorScheme(
     brightness: Brightness.light,
-    primary: Color(0xFFFDFDFD),
+    primary: Color(0xFF000000),
     onPrimary: Color(0xFFFFA74F),
-    secondary: Color(0xFFFDFDFD),
-    onSecondary: Color(0xFFFDFDFD),
+    secondary: Color(0xFF000000),
+    onSecondary: Color(0xFFFFFFFF),
     error: Color(0xFFF2CB57),
     onError: Color(0xFF850783),
     background: Color(0xFFFDFDFD),
     onBackground: Color(0xFFFFA74F),
     surface: Color(0xFFFFA74F),
-    onSurface: Color(0xFFFDFDFD),
+    onSurface: Color(0xFF000000),
   ),
 );
