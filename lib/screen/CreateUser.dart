7d38342515a5e2a0a5fec5f77ede2774d3c9834a
黒狗さん。 <kuroinusanshinji@gmail.com>
2024-09-05 20:56:54 +0900

    登録画面の訂正

--- lib/screen/CreateUser.dart
+++ lib/screen/CreateUser.dart
@@ -55,78 +55,80 @@ class _CreateUserScreenState extends State<CreateUserScreen> {
   }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Text("登録する",style: TextStyle(fontSize: 20),),
-          SizedBox(height: 20),
-          //アイコンとuidとunameの入力をする画面を構築する
-          CircleAvatar(
-            backgroundImage: imageFile == null ? const AssetImage('https://calendar-files.woody1227.com/user_icon/default.png') : FileImage(File(imageFile!.path)),
-            radius: 50,
-          ),
-          SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () async {
-              final XFile? image = await getImageFromGallery();
-              if (image != null) {
-                setState(() {
-                  imageFile = image;
-                });
-              }
-            },
-            child: Text("アイコンを選択"),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "@",
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("登録する",style: TextStyle(fontSize: 20),),
+            SizedBox(height: 20),
+            //アイコンとuidとunameの入力をする画面を構築する
+            CircleAvatar(
+              backgroundImage: imageFile == null ? const NetworkImage('https://calendar-files.woody1227.com/user_icon/default.png') : FileImage(File(imageFile!.path)),
+              radius: 50,
             ),
-            onChanged: (value) {
-              userInformation.userInformation.uid = value;
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "name",
-              hintText: "山田太郎",
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () async {
+                final XFile? image = await getImageFromGallery();
+                if (image != null) {
+                  setState(() {
+                    imageFile = image;
+                  });
+                }
+              },
+              child: Text("アイコンを選択"),
             ),
-            onChanged: (value) {
-              userInformation.userInformation.uname = value;
-            },
-          ),
-          ElevatedButton(
-            onPressed: () async{
-              //すべてが入力されているかどうか、uidが存在するかどうかを確認後、できていたらreturn
-              if (userInformation.userInformation.uid == "" || userInformation.userInformation.uname == "") {
-                //画面下にエラーを出す
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("すべての項目を入力してください"),
-                  ),
-                );
-                //uidがすでに存在するかどうか
-                try{
-                  await GetUser().getUser(userInformation.userInformation.uid);
-                } catch (e){
-                  //uidが存在する(404の)場合、エラーを出す
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "@",
+              ),
+              onChanged: (value) {
+                userInformation.userInformation.uid = value;
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "name",
+                hintText: "山田太郎",
+              ),
+              onChanged: (value) {
+                userInformation.userInformation.uname = value;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () async{
+                //すべてが入力されているかどうか、uidが存在するかどうかを確認後、できていたらreturn
+                if (userInformation.userInformation.uid == "" || userInformation.userInformation.uname == "") {
+                  //画面下にエラーを出す
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("このuidはすでに使われています"),
+                      content: Text("すべての項目を入力してください"),
                     ),
                   );
+                  //uidがすでに存在するかどうか
+                  try{
+                    await GetUser().getUser(userInformation.userInformation.uid);
+                  } catch (e){
+                    //uidが存在する(404の)場合、エラーを出す
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("このuidはすでに使われています"),
+                      ),
+                    );
+                    return;
+                  }
+                  if (imageFile != null){
+                    userInformation.imageFile = imageFile;
+                  }
                   return;
                 }
-                if (imageFile != null){
-                  userInformation.imageFile = imageFile;
-                }
-                return;
-              }
-              Navigator.pop(context, userInformation);
-            },
-            child: Text("登録"),
-          ),
-        ],
+                Navigator.pop(context, userInformation);
+              },
+              child: Text("登録"),
+            ),
+          ],
+        ),
       ),
     );
   }
